d622ac7ea9319524c90acec41beb69cf39900bcd
Ahtisham Ul Haq <77314350+ahtisham774@users.noreply.github.com>
2024-11-20 00:12:55 +0500

    update API

--- src/API.js
+++ src/API.js
@@ -1,3 +1,4 @@
-export const URI = 'http://localhost:8080';
+export const URI = 'https://phase-boggy-machine.glitch.me';
+// export const URI = 'http://localhost:8080';
 export const BASE_URL = `${URI}/api`;
 export const AUTH = `${BASE_URL}/auth`;
